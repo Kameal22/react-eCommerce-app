60b6c0aad94b7c61baae9c20a6a5f6b48e577503
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2022-02-07 15:28:44 +0100

    editing components

--- src/components/nav/NavMenu.tsx
+++ src/components/nav/NavMenu.tsx
@@ -6,12 +6,18 @@ const NavMenu: React.FC = () => {
   // If logged in - menu stays the same, if not - Create account, Log in, Cart
   return (
     <div className="navMenu">
-      <h5>Your account</h5>
-      <i className="bi bi-person"></i>
-      <h5>Your wishlist</h5>
-      <i className="bi bi-heart"></i>
-      <h5>Cart</h5>
-      <i className="bi bi-cart"></i>
+      <div className="yourAccDiv">
+        <h5>Your account</h5>
+        <i className="bi bi-person"></i>
+      </div>
+      <div className="yourWishDiv">
+        <h5>Your wishlist</h5>
+        <i className="bi bi-heart"></i>
+      </div>
+      <div className="cartDiv">
+        <h5>Cart</h5>
+        <i className="bi bi-cart"></i>
+      </div>
     </div>
   );
 };
