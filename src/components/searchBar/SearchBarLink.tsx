71f21779dadf86351a942a1b1f0924da16406238
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2022-03-09 11:13:57 +0100

    Fixing fetch-data bug

--- src/components/searchBar/SearchBarLink.tsx
+++ src/components/searchBar/SearchBarLink.tsx
@@ -5,27 +5,37 @@ import { Link } from "react-router-dom";
 interface searchBarProps {
   name: string;
   iconClass: string;
-  dropdowns: { name: string; category: string; idx: number }[] | { name: string; category: string; idx: string }[];
+  dropdowns: { name: string; category: string; idx: string | number }[];
 }
 
 const SearchBarLink: React.FC<searchBarProps> = (props) => {
   const [dropdownActive, setDropdownActive] = useState(false);
 
   return (
-    <div onMouseEnter={() => setDropdownActive(true)} onMouseLeave={() => setDropdownActive(false)} className="searchBarLinkDiv">
-      <Link
-        className="searchBarLink"
-        to={`/searchResult/${props.name}`}
-      >
+    <div
+      onMouseEnter={() => setDropdownActive(true)}
+      onMouseLeave={() => setDropdownActive(false)}
+      className="searchBarLinkDiv"
+    >
+      <Link className="searchBarLink" to={`/searchResult/${props.name}`}>
         <i className={props.iconClass} style={{ fontSize: "1.4em" }}></i>
         <p>{props.name}</p>
       </Link>
-      {dropdownActive ? <div className={'dropdownDiv'}>
-        {props.dropdowns.map((dropdown) => {
-          return <p className="dropdownItem"><Link to={`/searchResult/${props.name}/${dropdown.category}/${dropdown.idx}`}>{dropdown.name}</Link></p>
-        })}
-      </div> : null}
-
+      {dropdownActive ? (
+        <div className={"dropdownDiv"}>
+          {props.dropdowns.map((dropdown) => {
+            return (
+              <p className="dropdownItem">
+                <Link
+                  to={`/searchResult/${props.name}/${dropdown.category}/${dropdown.idx}`}
+                >
+                  {dropdown.name}
+                </Link>
+              </p>
+            );
+          })}
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/utills/FetchProductsFunc.tsx
+++ src/utills/FetchProductsFunc.tsx
@@ -2,44 +2,62 @@ import axios from "axios";
 import { Console } from "../interfaces/ConsoleInterface";
 import { Product } from "../interfaces/ProductInterface";
 
-export const fetchAndSetProductsFunc = async (productType: string | undefined, setProducts: React.Dispatch<React.SetStateAction<Console[]>> | React.Dispatch<React.SetStateAction<Product[]>>) => {
-
+export const fetchAndSetProductsFunc = async (
+  productType: string | undefined,
+  setProducts:
+    | React.Dispatch<React.SetStateAction<Console[]>>
+    | React.Dispatch<React.SetStateAction<Product[]>>
+) => {
   const response = await axios.get(`http://localhost:3000/${productType}`);
 
-  setProducts(response.data)
+  setProducts(response.data);
 };
 
-export const fetchAndFilterProductsFunc = async (productType: string | undefined, productCategory: string | number, productCategoryIdx: string | undefined, setProducts: React.Dispatch<React.SetStateAction<Console[]>> | React.Dispatch<React.SetStateAction<Product[]>>) => {
-
+export const fetchAndFilterProductsFunc = async (
+  productType: string | undefined,
+  productCategory: string | number,
+  productCategoryIdx: string | number | undefined, //CHANGIN THIS CAUSES AN ERROR. iT MUST BE STRING | UNDEFINED. BUT WHY?
+  setProducts: // | React.Dispatch<React.SetStateAction<Console[]>>
+  React.Dispatch<React.SetStateAction<Product[]>>
+) => {
   const response = await axios.get(`http://localhost:3000/${productType}`);
-  console.log(productCategoryIdx)
-  console.log(typeof (productCategoryIdx)) // Problem is, that this is a string for fuck sake.
+  console.log(productCategoryIdx);
+  console.log(typeof productCategoryIdx); // Problem is, that this is a string for fuck sake.
 
-  const filteredResponse = response.data.filter(((product: any) => { return product[productCategory] === productCategoryIdx }))
+  const filteredResponse = response.data.filter((product: any) => {
+    return product[productCategory] === productCategoryIdx;
+  });
 
-
-  console.log(filteredResponse)
-  console.log(response.data)
+  console.log(filteredResponse);
+  console.log(response.data);
   // setProducts(response.data)
 };
 
-export const fetchAndSetProductsFuncWithParams = async (productType: string | undefined, productId: string | undefined, setProduct: React.Dispatch<React.SetStateAction<Product | undefined>>) => {
+export const fetchAndSetProductsFuncWithParams = async (
+  productType: string | undefined,
+  productId: string | undefined,
+  setProduct: React.Dispatch<React.SetStateAction<Product | undefined>>
+) => {
+  const response = await axios.get(
+    `http://localhost:3000/${productType}/${productId}`
+  );
 
-  const response = await axios.get(`http://localhost:3000/${productType}/${productId}`);
-
-  setProduct(response.data)
+  setProduct(response.data);
 };
 
 export const fetchProducts = async (productType: string | undefined) => {
-
   const response = await axios.get(`http://localhost:3000/${productType}`);
 
-  return response.data
+  return response.data;
 };
 
-export const fetchSpecificProduct = async (productType: string | undefined, productId: string | undefined) => {
-
-  const response = await axios.get(`http://localhost:3000/${productType}/${productId}`);
+export const fetchSpecificProduct = async (
+  productType: string | undefined,
+  productId: string | undefined
+) => {
+  const response = await axios.get(
+    `http://localhost:3000/${productType}/${productId}`
+  );
 
   console.log(response.data);
-}
+};
